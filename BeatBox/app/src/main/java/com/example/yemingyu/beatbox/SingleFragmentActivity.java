d1680a84fe8054aedf4fced7ba0ce1b5cb2b4bef
夜禹 <mingyu.ymy@alipay.com>
2017-07-06 17:31:23 +0800

    beatbox

---
+++ BeatBox/app/src/main/java/com/example/yemingyu/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.example.yemingyu.beatbox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.yemingyu.beatbox.R;
+
+/**
+ * Created by yemingyu on 04/07/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (null == fragment) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+}
